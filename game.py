b4712acd31731e34a587c9415653dd954c621dcf
Adrià Bravo <adria.bravo@pizza.alea.group>
2018-08-17 09:49:55 +0200

    added while loop to game funcion and inventory display improved.

--- game.py
+++ game.py
@@ -5,20 +5,22 @@ def play():
 
     #game start
     print("Escape from Cave Terror!")
-    action_input = get_player_command()
-    if action_input in ['n', 'N']:
-        print("Go North!")
-    elif action_input in ['s', 'S']:
-        print('Go South!')
-    elif action_input in ['e', 'E']:
-        print('Go East!')
-    elif action_input in ['w', 'W']:
-        print('Go West!')
-    elif action_input in ['i', 'I']:
-        print("Inventory:")
-        print(inventory)
-    else:
-        print("invalid action!")
+    while True:
+        action_input = get_player_command()
+        if action_input in ['n', 'N']:
+            print("Go North!")
+        elif action_input in ['s', 'S']:
+            print('Go South!')
+        elif action_input in ['e', 'E']:
+            print('Go East!')
+        elif action_input in ['w', 'W']:
+            print('Go West!')
+        elif action_input in ['i', 'I']:
+            print("Inventory:")
+            for item in inventory:
+                print('*' + str(item))
+        else:
+            print("invalid action!")
 
 def get_player_command():
     return raw_input('Action: ')
